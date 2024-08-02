8e8f254c6c2f5b85bee0af19d7316c295b1c1d97
Nils Nahooy <nils.nahooy@live.com>
2024-08-02 20:10:29 +0200

    states and flip on move left

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -11,9 +11,11 @@ const HURT_TIME: float = 0.3
 const JUMP_VELOCITY: float = -400.0
 
 enum PLAYER_STATE {IDLE, RUNNING, FALLING, JUMPING, HURT}
+enum PLAYER_DIRECTION {LEFT, RIGHT}
 
 var _gravity: float = ProjectSettings.get("physics/2d/default_gravity")
 var _player_state: PLAYER_STATE = PLAYER_STATE.IDLE
+var _player_direction: PLAYER_DIRECTION = PLAYER_DIRECTION.RIGHT
 
 func _physics_process(delta: float) -> void:
 	if !is_on_floor():
@@ -21,19 +23,52 @@ func _physics_process(delta: float) -> void:
 
 	_get_input()
 	move_and_slide()
+	_calculate_state()
 
 func _get_input() -> void:
 	velocity.x = 0
 	if Input.is_action_pressed("left"):
 		velocity.x = -RUN_SPEED
+		_player_direction = PLAYER_DIRECTION.LEFT
 	elif Input.is_action_pressed("right"):
 		velocity.x = RUN_SPEED
+		_player_direction = PLAYER_DIRECTION.RIGHT
 	
 	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	velocity.y = clampf(velocity.y, JUMP_VELOCITY, MAX_FALL_SPEED)
+		
 
 func _calculate_state() -> void:
 	if _player_state == PLAYER_STATE.HURT:
 		return
+
+	if is_on_floor():
+		if velocity.x == 0:
+			set_state(PLAYER_STATE.IDLE)
+		else:
+			set_state(PLAYER_STATE.RUNNING)
+	else:
+		if velocity.y > 0:
+			set_state(PLAYER_STATE.FALLING)
+		else:
+			set_state(PLAYER_STATE.JUMPING)
+
+func set_state(new_state: PLAYER_STATE) -> void:
+	if new_state == _player_state:
+		return
+
+	_player_state = new_state
+
+	match _player_state:
+		PLAYER_STATE.IDLE:
+			animation_player.play("idle")
+		PLAYER_STATE.RUNNING:
+			animation_player.play("run")
+		PLAYER_STATE.JUMPING:
+			animation_player.play("jump")
+		PLAYER_STATE.FALLING:
+			animation_player.play("fall")
+			
+	sprite.flip_h = _player_direction == PLAYER_DIRECTION.LEFT
