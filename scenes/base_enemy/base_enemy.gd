9d39142af17543c04d3cfdf74927297c124d0a3f
Nils Nahooy <nils.nahooy@live.com>
2024-08-04 11:57:33 +0200

    snail movement

--- scenes/base_enemy/base_enemy.gd
+++ scenes/base_enemy/base_enemy.gd
@@ -8,10 +8,10 @@ enum FACING {LEFT = -1, RIGHT = 1}
 
 @export var default_facing: FACING = FACING.LEFT
 @export var points: int = 1
+@export var speed: float = 30.0
 
-var _speed: float = 30.0
-var _gravity: float = 800.0
-var _facing: FACING = default_facing
+var gravity: float = 800.0
+var facing: FACING = default_facing
 var _player_ref: Player
 var _dead: bool = false
 
--- scenes/snail/snail.gd
+++ scenes/snail/snail.gd
@@ -1,1 +1,27 @@
 extends Enemy
+
+@onready var floor_detection: RayCast2D = $"FloorDetection"
+@onready var animated_sprite: AnimatedSprite2D = $"AnimatedSprite2D"
+
+func _physics_process(delta: float) -> void:
+	super._physics_process(delta)
+
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		print("test")
+	else:
+		velocity.x = speed * facing
+
+	move_and_slide()
+
+	if is_on_floor():
+		if is_on_wall() or not floor_detection.is_colliding():
+			_flip()
+
+func _flip() -> void:
+	animated_sprite.flip_h = not animated_sprite.flip_h
+	floor_detection.position.x = floor_detection.position.x * -1
+	if facing == FACING.LEFT:
+		facing = FACING.RIGHT
+	else:
+		facing = FACING.LEFT
