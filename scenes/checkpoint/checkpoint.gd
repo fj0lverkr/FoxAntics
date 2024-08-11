21926b44ac60fde136288002961385103e218098
Nils Nahooy <nils.nahooy@live.com>
2024-08-11 12:11:01 +0200

    implement checkpoint

---
+++ scenes/checkpoint/checkpoint.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+class_name Checkpoint
+
+const TRIGGER: String = "parameters/conditions/on_trigger"
+
+@onready var state_machine: AnimationTree = $"AnimationTree"
+@onready var sound: AudioStreamPlayer2D = $"Sound"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	SignalBus.on_boss_killed.connect(_on_boss_killed)
+
+func _on_boss_killed(_points: int) -> void:
+	if not state_machine[TRIGGER]:
+		state_machine[TRIGGER] = true
+		monitoring = true
+		SoundManager.play_sound_2d(sound, SoundManager.YOU_WIN)
+
+func _on_area_entered(_area: Area2D) -> void:
+	print("LEVEL COMPLETE")
