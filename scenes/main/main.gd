d6786e3477496042277de18255f88ada121232bb
Nils Nahooy <nils.nahooy@live.com>
2024-08-20 20:28:53 +0200

    add highscore to main screen

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,7 +1,12 @@
 extends CanvasLayer
 
+@onready var high_score_label: Label = $VBoxContainer/HBoxContainer/HSLabel
+
 func _ready() -> void:
 	Engine.time_scale = 1
+	var hs: int = ScoreManager.get_high_score()
+	if hs > 0:
+		high_score_label.text = str(hs)
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("up"):
--- singletons/score_manager.gd
+++ singletons/score_manager.gd
@@ -7,45 +7,51 @@ var _score: int = 0
 var _score_history: Array = []
 
 func _ready() -> void:
-    _reset_score()
-    SignalBus.on_boss_killed.connect(_on_points_added)
-    SignalBus.on_enemy_hit.connect(_on_points_added)
-    SignalBus.on_pickup_taken.connect(_on_points_added)
-    SignalBus.on_game_over.connect(_on_game_over)
-    _load_score()
+	_reset_score()
+	SignalBus.on_boss_killed.connect(_on_points_added)
+	SignalBus.on_enemy_hit.connect(_on_points_added)
+	SignalBus.on_pickup_taken.connect(_on_points_added)
+	SignalBus.on_game_over.connect(_on_game_over)
+	_load_score()
 
 func _on_points_added(points: int) -> void:
-    _score += points
-    SignalBus.on_score_updated.emit(_score)
+	_score += points
+	SignalBus.on_score_updated.emit(_score)
 
 func _on_game_over() -> void:
-    _score_history.append({"score": _score})
-    _save_score()
+	_score_history.append({"score": _score})
+	_save_score()
 
 func _load_score() -> void:
-    if not FileAccess.file_exists(SCORE_PATH):
-        return
-    var file: FileAccess = FileAccess.open(SCORE_PATH, FileAccess.READ)
-    var text: String = file.get_as_text()
-    if not text or text.length() <= 0:
-        return
-    _score_history = JSON.parse_string(text)
-    file.close()
+	if not FileAccess.file_exists(SCORE_PATH):
+		return
+	var file: FileAccess = FileAccess.open(SCORE_PATH, FileAccess.READ)
+	var text: String = file.get_as_text()
+	if not text or text.length() <= 0:
+		return
+	_score_history = JSON.parse_string(text)
+	file.close()
 
 func _save_score() -> void:
-    _score_history.sort_custom(_sort_scores)
-    if _score_history.size() > MAX_SCORES:
-        _score_history = _score_history.slice(0, MAX_SCORES)
-    var file: FileAccess = FileAccess.open(SCORE_PATH, FileAccess.WRITE)
-    if file:
-        file.store_string(JSON.stringify(_score_history))
-        file.close()
+	_score_history.sort_custom(_sort_scores)
+	if _score_history.size() > MAX_SCORES:
+		_score_history = _score_history.slice(0, MAX_SCORES)
+	var file: FileAccess = FileAccess.open(SCORE_PATH, FileAccess.WRITE)
+	if file:
+		file.store_string(JSON.stringify(_score_history))
+		file.close()
 
 func _reset_score() -> void:
-    _score = 0
+	_score = 0
 
 func _sort_scores(a: Dictionary, b: Dictionary) -> bool:
-    return a.score > b.score
+	return a.score > b.score
 
 func get_score() -> int:
-    return _score
+	return _score
+
+func get_high_score() -> int:
+	if _score_history.size() == 0:
+		return 0
+	_score_history.sort_custom(_sort_scores)
+	return _score_history.front().score
